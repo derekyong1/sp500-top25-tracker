8c950137d5a4b01a13ddee554e71f42da34c7cd9
Derek Yong <dy0291@gmail.com>
2024-02-25 17:17:38 -0500

    added 25 companies and added logic to send the api info to df

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
+# Module imports
 from flask import Flask, render_template
 import requests
 import pandas as pd
+
+# Function imports
+from company_list import get_top_companies as companies
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     api_key = "05166bc463a471432f1ff89803b15568"
-    url = f"https://financialmodelingprep.com/api/v3/profile/AAPL,GOOGL,MSFT?apikey={api_key}"  # Example URL
-    response = requests.get(url)
-    data = response.json()
-    print(data)
+
+    all_data = []
+    company_identifiers = companies()
+    for i in range(len(company_identifiers)):
+        url = f"https://financialmodelingprep.com/api/v3/market-capitalization/{company_identifiers[i]}?apikey={api_key}" 
+
+        response = requests.get(url)
+        data = response.json()
+        all_data.extend(data)
+
     # Convert to DataFrame
-    df = pd.DataFrame(data)
+    df = pd.DataFrame(all_data)
     
     # Assuming the API returns market cap, sort by 'marketCap' and select top 25 companies
     # Note: Adjust the field name 'marketCap' based on actual response
-    top_companies = df.sort_values(by='mktCap', ascending=False).head(25)
-
+    top_companies = df.sort_values(by='marketCap', ascending=False).head(25)
+    
     return render_template('index.html', companies=top_companies.to_dict('records'))
 
 if __name__ == '__main__':
---
+++ company_list.py
@@ -0,0 +1,34 @@
+def get_top_companies():
+    companies = [
+        "AAPL",
+        "MSFT",
+        "GOOGL",
+        "AMZN",
+        "NVDA",
+        "META",
+        "BRK.B",
+        "TSLA",
+        "LLY",
+        "V",
+        "UNH",
+        "AVGO",
+        "JPM",
+        "WMT",
+        "XOM",
+        "MA",
+        "JNJ",
+        "PG",
+        "HD",
+        "MRK",
+        "COST",
+        "CVX",
+        "ABBV",
+        "ORCL",
+        "BAC"
+    ]
+
+    return companies
+
+
+
+
